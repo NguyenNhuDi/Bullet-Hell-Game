91cfff4406e6ee4a7305eb191b2d7cab9f93f4af
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-12-26 12:44:55 -0700

    turn off game with escape

--- bullet_hel_game/main.py
+++ bullet_hel_game/main.py
@@ -4,18 +4,22 @@ pygame.init()
 
 SWIDTH = 800
 SHEIGHT = 600
+TITLE = 'Bullet Hell'
 
 if __name__ == '__main__':
     screen = pygame.display.set_mode((SWIDTH, SHEIGHT))
-
+    pygame.display.set_caption(f'{TITLE: ^100}')
     clock = pygame.time.Clock()
 
     while True:
+        keys = pygame.key.get_pressed()
         # Process player inputs.
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
                 pygame.quit()
                 raise SystemExit
+
+
 
         # Do logical updates here.
         # ...
